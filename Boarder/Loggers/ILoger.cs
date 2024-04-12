5e2203e79272ca566d384b0e224263c17fc9382d
Iva Apostolova <iveto9595@gmail.com>
2024-04-12 12:18:32 +0300

    Add project files.

---
+++ Boarder/Loggers/ILoger.cs
@@ -0,0 +1,14 @@
+﻿using Boarder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boarder.Loggers
+{
+    public interface ILogger
+    {
+        void Log(string value);
+    }
+}
---
+++ Boarder/Program.cs
@@ -0,0 +1,28 @@
+﻿using Boarder.Loggers;
+using Boarder.Models;
+using System;
+
+namespace Boarder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var tomorrow = DateTime.Now.AddDays(1);
+            //BoardItem task = new Task("Write unit tests", "Pesho", tomorrow);
+            //BoardItem issue = new Issue("Review tests", "Someone must review Peter's tests.", tomorrow);
+
+            //Console.WriteLine(task.ViewInfo());
+            //Console.WriteLine(issue.ViewInfo());
+
+            var tomorrow = DateTime.Now.AddDays(1);
+            var task = new Task("Write unit tests", "Peter", tomorrow);
+            var issue = new Issue("Review tests", "Someone must review Peter's tests.", tomorrow);
+            Board.AddItem(task);
+            Board.AddItem(issue);
+
+            ConsoleLogger logger = new ConsoleLogger();
+            Board.LogHistory(logger); // pass a ConsoleLogger type where an ILogger is expected:
+        }
+    }
+}
